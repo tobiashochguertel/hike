9f7ac9987e66282dcecc79652d38d755292caa4e
Dave Pearson <davep@davep.org>
2025-04-24 07:43:05 +0100

    :hammer: Swap to using History provided by textual_enhanced

--- src/hike/support/__init__.py
+++ src/hike/support/__init__.py
@@ -2,12 +2,11 @@
 
 ##############################################################################
 # Local imports.
-from .history import History
 from .mouse import is_copy_request_click
 from .view_in_browser import view_in_browser
 
 ##############################################################################
 # Exports.
-__all__ = ["History", "is_copy_request_click", "view_in_browser"]
+__all__ = ["is_copy_request_click", "view_in_browser"]
 
 ### __init__.py ends here
